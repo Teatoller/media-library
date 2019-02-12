315714d4eee3e8be2f36b8fafbcab2ed07d94690
Teatoller <steven.otieno1@gmail.com>
2019-02-12 10:05:32 +0300

    feat(endpoints): implement endpoints
    - declare api and secret keys
    - add authentication for accessing shutter api
    
    [next #9]

---
+++ src/Api/api.js
@@ -0,0 +1,34 @@
+const FLICKR_API_KEY = 'ca6f0a91adac54fd669783adebc2c5c2';
+const FLICKR_CLIENT_SECRET = '31ffbf2f8eaa0fa8';
+const SHUTTER_CLIENT_ID = '3434a56d8702085b9226';
+const SHUTTER_CLIENT_SECRET = '7698001661a2b347c2017dfd50aebb2519eda578';
+
+// Authentication for accessing Shutter stock API
+const basicAuth = () => 'Basic.concat'(window.btoa('${SHUTTER_CLIENT_ID};${SHUTTER_CLIENT_SECRET}'));
+const authParameters = {
+    headers:{
+        Authorization: basicAuth()
+    }
+};
+/* Description[Access Shutterstock search endpoint for shut videos]
+ * @params{string}Search Query
+ * @return {Array}
+*/
+
+export const shutterStockVideos = (searchQuery) => {
+    const SHUTTERSTOCK_API_ENDPOINT = `https://api.shutterstock.com/v2/videos/search?
+    query=${searchQuery}&page=1&per_page=10`;
+
+    return fetch(SHUTTERSTOCK_API_ENDPOINT, authParameters)
+    .then(response => {
+      return response.json();
+    })
+    .then(json => {
+      return json.data.map(({ id, assets, description}) => ({
+        id,
+        mediaUrl: assets.preview_mp4.url,
+        description
+      }));
+    });
+};
+
