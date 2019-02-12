05af6ec217685242ab40de95a8e1053b91955cb4
Teatoller <steven.otieno1@gmail.com>
2019-02-12 21:20:56 +0300

    feat(actiontypes): implement actiontypes
    - declare action types constants/actionTypes.js
    
    [next #13]

---
+++ src/constants/actionTypes.js
@@ -0,0 +1,11 @@
+// Action types functions that return plain javascript
+// and an optional payload that are dispatched to store
+export const SELECTED_IMAGE = 'SELECTED_IMAGE';
+export const FLICKR_IMAGES_SUCCESS = 'FLICKER_IMAGES_SUCCESS';
+export const SELECTED_VIDEO = 'SELECTED_VIDEO';
+export const SHUTTER_VIDEOS_SUCCESS = 'SHUTTER_VIDEO_SUCCESS';
+export const SEARCH_MEDIA_REQUEST = 'SEARCH_MEDIA_REQUEST';
+export const SEARCH_MEDIA_SUCCESS = 'SEARCH_MEDIA_SUCCESS';
+export const SEARCH_MEDIA_ERROR = 'SERACH_MEDIA_ERROR';
+
+
