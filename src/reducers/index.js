0539aa75a82f54306d21042024fda9f2a1548e98
Teatoller <steven.otieno1@gmail.com>
2019-02-13 07:16:19 +0300

    feat(reducers): implement root reducer
    - combine all reducers to a single reducer function
    
    [next #17]

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,1 +1,11 @@
+import { combineReducers } from 'redux';
+import images from '/imageReducer';
+import videos from './videoReducer';
 
+// Combine all reducersto a single reducer function
+const rootReducer = combineReducers({
+    images,
+    videos
+});
+
+export default rootReducer;
