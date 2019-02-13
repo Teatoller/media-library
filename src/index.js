caf718c6916d972786ed560521642bd2ea021956
Teatoller <steven.otieno1@gmail.com>
2019-02-13 13:11:55 +0300

    feat(store): initialize store
    - add method to access store with connect method
    - add provider component from react-redux
    
    [next #21]

--- src/index.js
+++ src/index.js
@@ -2,20 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes'
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
+
+// Initialize store
+const store = configureStore();
 
 ReactDOM.render(
-    <Router history={browserHistory} routes={routes} />,
+    <Provider store={store}>
+        <Router history={browserHistory} routes={routes} />
+    </Provider>,
     document.getElementById('root')
 );
-
-
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import { Router, browserHistory } from 'react-router';
-// import routes from './routes';
-
-// // We require the routes and render to the DOM using ReactDOM API
-// ReactDOM.render(
-//     <Router history={browserHistory} routes={routes} />, 
-//     document.getElementById('root')
-// );
